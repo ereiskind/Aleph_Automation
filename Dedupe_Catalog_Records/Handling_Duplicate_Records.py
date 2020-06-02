6691c5f0568fed59e6dea81ca9e3e61aec9c3287
Elissa Reiskind <ereiskind@fsu.edu>
2020-06-02 12:07:47 -0400

    Create folder Dedupe_Catalog_Records

---
+++ Dedupe_Catalog_Records/Handling_Duplicate_Records.py
@@ -0,0 +1,6 @@
+# Process for handling having multiple records for a given owned title in UXU01 and possibly UXU60
+
+# 1. Collect all possible BIB numbers for the platform/collection in question
+# 2. Send BIBs to cataloging to have them check what ones have ACQ records attached
+# 4. Pull HOL for BIBs and ???
+# 3. Pull all 856$u to get propriatary IDs and determine what BIBs are duplicates
