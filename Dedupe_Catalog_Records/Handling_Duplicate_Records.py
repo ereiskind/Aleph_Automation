e88b8c5dd1cd9a5bf42f024ca5cf0a474944e7d5
Elissa_Reiskind <ereiskind@fsu.edu>
2020-06-04 11:39:09 -0400

    Recreate psuedocode

--- Dedupe_Catalog_Records/Handling_Duplicate_Records.py
+++ Dedupe_Catalog_Records/Handling_Duplicate_Records.py
@@ -3,12 +3,35 @@
 import os
 from tkinter import messagebox
 
-# 1. Collect all possible BIB numbers for the platform/collection in question
-# 2. Send BIBs to cataloging to have them check what ones have ACQ records attached
-# 3. Pull HOL for BIBs and get TKR, if suppressed
-# tkinter.messagebox.showwarning(title=None, message=None, **options) that JSON is for Ebook Central
-# 4. Pull all 856$u to get propriatary IDs and determine what BIBs are duplicates
-# 5. Pull ISBNs from 020 and 776
-# 6. Determine duplicates
-# 7. For keeps, check URL, TKR--provide alpeh sequential to update if needed
-# 8. For removals, check if other HOLs--if yes, say delete HOL, if no, say delete BIB and HOL
+#Section: Determine What BIBs Have ACQ Records
+#Subsection: Collect all possible BIB numbers for the platform/collection in question
+
+#Subsection: Send those BIBs to cataloging to have them check which ones have ACQ records attached
+
+
+#Section: Collect BIB Information
+#Subsection: Pull Info from Aleph
+# For BIBs, print-03 Aleph sequential of 856 fields
+# For BIBs, manage-70 for BIB-to-HOL
+# For HOLs, print-03 Aleph sequential of TKR, LKR, STA
+
+#Subsection: Organize Aleph Output
+# before JSON launch: tkinter.messagebox.showwarning(title=None, message=None, **options) that JSON is for Ebook Central
+
+
+#Section: Determine Which Records to Keep
+#Subsection: Match Duplicate BIB Records
+
+#Subsection: Determine Which BIB Records to Keep
+
+
+#Section: Create Update Files For Records to Remain
+#Subsection: If New 856$u Needed, Supply It
+
+#Subsection: If TKR Needed, Supply It
+
+
+#Section: Remove Unneeded Records
+#Subsection: If Other HOLs Attached to BIB, Delete FSUER HOL
+
+#Subsection: If FSUER HOL is Only HOL, Delete HOL and BIB
